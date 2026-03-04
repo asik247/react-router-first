f8eb0d06e7aeefb34490fdea25b4d067a7153c23
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-04 14:47:54 +0600

    hello show usign router provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<h1>Hello react router</h1>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
