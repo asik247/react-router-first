ee4c879c74c43bcfdd5a1966d6a5e2db1520cc5a
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-04 15:03:16 +0600

    route code implement

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import Navbar from './Components/Navbar/Navbar.jsx'
+// import Navbar from './Components/Navbar/Navbar.jsx'
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<h1>Hello react router</h1>
+    // element:<h1>Hello react router</h1>
+    // element:<Navbar></Navbar>
+    // Component:Navbar
+    Component:Navbar
   }
 ])
 
