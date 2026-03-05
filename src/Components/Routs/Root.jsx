42e7ae308774898490c5d840801628e7b64cd808
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-05 12:36:26 +0600

    useNavigiton code explore

--- src/Components/Routs/Root.jsx
+++ src/Components/Routs/Root.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 
 const Root = () => {
+    // use navigation code start here;
+    const navigation = useNavigation();
+    const isNavigation = Boolean(navigation.location);
     return (
         <div>
             <Navbar></Navbar>
             <div className='w-full h-100vh'>
+                {isNavigation && <h1>Loadding...</h1>}
                 <Outlet></Outlet>
 
             </div>
